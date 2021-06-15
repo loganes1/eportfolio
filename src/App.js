006f4d26674eadd6932a1e63ec023f2cf98ad3cc
Logan <lsaunders275@gmail.com>
2021-06-14 21:12:54 -0600

    create react app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css';
-import Nav from "./Nav/Nav";
-import Hamburger from './Nav/Hamburger';
-import Home from "./Home/Home";
-import Skills from "./Skills/Skills";
-import WorkExp from "./WorkExp/WorkExp";
+import logo from './logo.svg';
+import * as styles from './App.module.scss';
 
-export default class App extends React.Component {
-  toggleNavIsActive() {
-    // toggle from a hamburger to an 'X'
-    const hamburger = document.getElementById("hamburger");
-    hamburger.classList.toggle("is-active");
-    // show/hide the menu
-    const nav = document.getElementById("nav");
-    nav.classList.toggle("open");
-    const main = document.querySelector(".app");
-    main.classList.toggle("open");
-  }
-
-  render() {
-    return <Router>
-      <Hamburger onClick={this.toggleNavIsActive} />
-      <Nav onClick={this.toggleNavIsActive} />
-
-      <main className="app">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/skills">
-            <Skills />
-          </Route>
-          <Route path="/projects">
-          </Route>
-          <Route path="/work-experience">
-            <WorkExp />
-          </Route>
-        </Switch>
-      </main>
-    </Router>
-  }
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className={styles.link}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
